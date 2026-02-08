9250c8ad59dce92032edc61095a482ab10b3bc28
Abhijit-Dev-app <solemrec@gmail.com>
2026-02-08 19:11:56 +0530

    Add files via upload

---
+++ new-folder/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const alertRoutes = require("./routes/alerts");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/alerts", alertRoutes);
+
+app.listen(5000, () => {
+  console.log("SOAR backend running on http://localhost:5000");
+});
